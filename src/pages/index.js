dbd23411bede126dda5976a0e5ab14927975e91f
Veronica Bills <vbills123@gmail.com>
2019-12-14 17:39:06 -0500

    adding all our new blog code for gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,18 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color: blue;
+`
 
 export default ({ data }) => {
   return (
@@ -14,9 +23,12 @@ export default ({ data }) => {
         <h4>{data.allMarkdownRemark.totalCount}</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
-            <span>
-              {node.frontmatter.title} - {node.frontmatter.date}{" "}
-            </span>
+            <BlogLink to={node.fields.slug}>
+              <BlogTitle>
+                {node.frontmatter.title} - {node.frontmatter.date}{" "}
+              </BlogTitle>
+            </BlogLink>
+
             <p>{node.excerpt}</p>
           </div>
         ))}
@@ -27,7 +39,7 @@ export default ({ data }) => {
 
 export const query = graphql`
   query {
-    allMarkdownRemark {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       totalCount
       edges {
         node {
@@ -37,7 +49,9 @@ export const query = graphql`
             title
             date
           }
-          html
+          fields {
+            slug
+          }
           excerpt
         }
       }
